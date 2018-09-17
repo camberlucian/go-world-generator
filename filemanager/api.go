30482326139e5cd88a8d71e23bdb4e30f23b46e1
camberlucian <cambergriffin@gmail.com>
2018-09-16 20:48:28 -0400

    first commit, file manager

---
+++ filemanager/api.go
@@ -0,0 +1,34 @@
+package filemanager
+
+import (
+	"errors"
+	"os"
+)
+
+func writeStringsToFile(input []string, fileName string) error {
+	file, err := getOrCreateFile(fileName)
+	if err != nil {
+		return errors.New("writeStringToFileError: " + err.Error())
+	}
+	defer file.Close()
+	for _, str := range input {
+		_, err := file.WriteString(str)
+		if err != nil {
+			return errors.New("writeStringToFileError: " + err.Error())
+		}
+	}
+	file.Sync()
+	return nil
+}
+
+func getOrCreateFile(fileName string) (file *os.File, err error) {
+	if _, err = os.Stat("files/" + fileName); os.IsNotExist(err) {
+		file, err = os.Create("files/" + fileName)
+	} else {
+		file, err = os.Open("files/" + fileName)
+	}
+	if err != nil {
+		return file, errors.New("GetOrCreateFileError: " + err.Error())
+	}
+	return file, nil
+}
